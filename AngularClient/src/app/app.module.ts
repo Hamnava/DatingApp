ca249c5c830ef83bf80ed59be30cb53d79151fd9
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-02-19 22:14:35 -0800

    login functionality implemented in the angular page

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularClient/src/app/nav/nav.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+model : any = {}
+loggedIn:boolean
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login (){
+   this.accountService.login(this.model).subscribe(response=> {
+     console.log(response);
+     this.loggedIn = true;
+   }, error=> {
+    console.log(error);
+   })
+  }
+
+
+  logout(){
+    this.loggedIn= false;
+  }
+}
