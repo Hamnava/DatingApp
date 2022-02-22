c27228683c1f415c5a2a3f1aac1530a00c433b98
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-02-21 16:39:19 -0800

    routing guard added to navigation links

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -5,13 +5,22 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './member/member-detail/member-detail.component';
 import { MemberListComponent } from './member/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guard/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent},
-  {path: 'members/:id', component: MemberDetailComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component: MemberListComponent},
+      {path: 'members/:id', component: MemberDetailComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent},
+    ]
+  },
+
   {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
 
