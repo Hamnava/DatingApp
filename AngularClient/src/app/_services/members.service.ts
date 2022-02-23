723f14063a9c9d5cc60d125c405e7b343f0a0327
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-02-23 10:42:30 -0800

    user cards desinged for showing

--- AngularClient/src/app/_services/members.service.ts
+++ AngularClient/src/app/_services/members.service.ts
@@ -1,13 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-const httpOptions = {
- headers : new HttpHeaders ({
-   Authorization: 'Bearer '+ JSON.parse(localStorage.getItem('user'))?.token
- })
-}
 
 @Injectable({
   providedIn: 'root'
@@ -21,10 +16,10 @@ export class MembersService {
 
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'user' , httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'user' );
   }
 
   getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + 'user/'+ username, httpOptions );
+    return this.http.get<Member>(this.baseUrl + 'user/'+ username );
   }
 }
--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { MemberCardComponent } from './member/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 
 
@@ -36,7 +38,8 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     ListsComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -47,7 +50,8 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     SharedModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true},
   ],
   bootstrap: [AppComponent]
 })
