e4fb250d04b700d317a58687f23f4cf7b8663912
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-02-23 16:15:21 -0800

    update member of the application implemented in the api project

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -31,17 +32,27 @@ namespace API.Controllers
             
         }
 
-        //[HttpGet]
-        //[Route("{id}")]
-        //public async Task<ApplicationUser> GetUser(int id)
-        //{
-        //    return await _context.GetUserByIdAsync(id);
-        //}
+        
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDTO>> GetUserByUsername(string username)
         {
             return await _context.GetMemberByUsernameAsync(username);
            
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(UpdateMemberDto memberDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _context.GetUserByUsernameAsync(username);
+            _mapper.Map(memberDto, user);
+
+             _context.UpdateUser(user);
+             //await _context.SaveAllAsync();
+
+            if (await _context.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Faild to Update user!");
         }
     }
 }
--- API/Helpers/AutoMapperProfile.cs
+++ API/Helpers/AutoMapperProfile.cs
@@ -15,6 +15,7 @@ namespace API.Helpers
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotoDTO>().ReverseMap();
+            CreateMap<UpdateMemberDto, ApplicationUser>();
         }
     }
 }
