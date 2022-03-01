42fc8d4e906c911db3a7ffb230e71398fb88070a
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-02-28 17:11:32 -0800

    member likes implemeted in the angular part

--- AngularClient/src/app/_services/members.service.ts
+++ AngularClient/src/app/_services/members.service.ts
@@ -112,4 +112,12 @@ export class MembersService {
   deletePhoto(photoId:number){
     return this.http.delete(this.baseUrl + 'user/delete-photo/' + photoId);
   }
+
+  addLike(username: string){
+    return this.http.post(this.baseUrl + 'likes/'+ username, {});
+  }
+
+  getLikes(predicate: string){
+    return this.http.get<Partial<Member[]>>(this.baseUrl + 'likes?predicate='+ predicate);
+  }
 }
--- AngularClient/src/app/member/member-card/member-card.component.ts
+++ AngularClient/src/app/member/member-card/member-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-card',
@@ -8,10 +10,16 @@ import { Member } from 'src/app/_models/member';
 })
 export class MemberCardComponent implements OnInit {
  @Input() member: Member
- 
-  constructor() { }
+
+  constructor(private memberService: MembersService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
+
+  addLike(member:Member){
+    this.memberService.addLike(member.userName).subscribe(()=> {
+      this.toastr.success("You have liked "+ member.knownAs);
+    })
+  }
 }
