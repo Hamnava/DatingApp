5a24f8f048aa530d379b3e87c24441bcca863a5d
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-02-28 21:59:39 -0800

    controller created for message

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Repository.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MessagesController : BaseAPIController
+    {
+        private readonly UserInterface _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        public MessagesController(UserInterface userInterface, IMessageRepository messageRepository)
+        {
+            _userRepository = userInterface;
+            _messageRepository = messageRepository;
+        }
+
+        
+
+    }
+}
