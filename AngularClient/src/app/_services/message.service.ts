1d630320dc5aea822428e3cebdb215ab55601335
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-03-03 14:28:26 -0800

    message tracking implemented

--- AngularClient/src/app/_services/message.service.ts
+++ AngularClient/src/app/_services/message.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Messages } from '../_models/message';
+import { User } from '../_models/user';
 import { getPaginatedResult, getPaginationHeader } from './PaginationHelper';
 
 @Injectable({
@@ -9,8 +13,40 @@ import { getPaginatedResult, getPaginationHeader } from './PaginationHelper';
 })
 export class MessageService {
  baseUrl = environment.apiUrl;
+ hubUrl = environment.hubUrl;
+ hubConnection: HubConnection;
+ private messageThreadSource= new BehaviorSubject<Messages[]>([]);
+ messageThread$ = this.messageThreadSource.asObservable();
 
   constructor(private http: HttpClient) { }
+
+
+  createHubConnection(user: User, otherUsername:string){
+      this.hubConnection = new HubConnectionBuilder()
+              .withUrl(this.hubUrl + 'message?user='+ otherUsername, {
+                accessTokenFactory: ()=> user.token
+              })
+              .withAutomaticReconnect()
+              .build();
+
+      this.hubConnection.start().catch(error=> console.log(error));
+
+      this.hubConnection.on('ReceiveMessageThread', messages=> {
+        this.messageThreadSource.next(messages);
+      });
+
+      this.hubConnection.on('NewMessage', message => {
+        this.messageThread$.pipe(take(1)).subscribe(messages=> {
+          this.messageThreadSource.next([...messages, message]);
+        })
+      })
+  }
+
+  stopHubConnection(){
+    if(this.hubConnection){
+      this.hubConnection.stop();
+    }
+  }
 
   getMessages(pageNumber, pageSize, container){
      let params = getPaginationHeader(pageNumber, pageSize);
@@ -24,7 +60,8 @@ export class MessageService {
   }
 
   sendMessage(username:string, content:string){
-    return this.http.post<Messages>(this.baseUrl + 'messages', {recipeintUsername: username, content})
+    return this.hubConnection.invoke('SendMessage', {recipeintUsername: username, content})
+        .catch(error=> console.log(error))
   }
 
   deleteMessage(id: number){
--- AngularClient/src/app/member/member-messages/member-messages.component.ts
+++ AngularClient/src/app/member/member-messages/member-messages.component.ts
@@ -16,14 +16,13 @@ export class MemberMessagesComponent implements OnInit {
  messageContent:string;
 
 
-  constructor(private messageService: MessageService) { }
+  constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
   sendMessage(){
-    this.messageService.sendMessage(this.username, this.messageContent).subscribe(message=> {
-      this.messages.push(message);
+    this.messageService.sendMessage(this.username, this.messageContent).then(()=> {
       this.messageForm.reset();
     })
   }
